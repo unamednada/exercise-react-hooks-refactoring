19a7e7ce625ff3e3bcaff444ed60d0e9dd6b0eb7
unamednada <dias.gustavo.dias@gmail.com>
2021-11-01 22:57:46 -0300

    Refactor to use function usestate and usecontext

--- src/Cars.jsx
+++ src/Cars.jsx
@@ -1,15 +1,13 @@
 // src/Cars.jsx
 
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import CarsContext from './context/CarsContext';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 
-class Cars extends Component {
-  render() {
-    const { redCar, blueCar, yellowCar } = this.context.cars;
-    const { moveCar } = this.context;
+function Cars () {
+    const { cars: { redCar, yellowCar, blueCar }, moveCar } = useContext(CarsContext);
     return (
       <div>
         <div>
@@ -53,9 +51,6 @@ class Cars extends Component {
         </div>
       </div>
     )
-  }
 };
-
-Cars.contextType = CarsContext;
 
 export default Cars;
